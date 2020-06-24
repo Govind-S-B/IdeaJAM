4efc3cfd71b33629d796cdfbc94fa18decfb0454
Rex-the-mess <63335469+Rex-the-mess@users.noreply.github.com>
2020-06-24 20:42:20 +0530

    Ohey there's no saves lmao.

--- RPG.py
+++ RPG.py
@@ -2,7 +2,7 @@ import random
 
 hp = 100
 kill_count = 0
-enemy_hp = 0
+enemy_hp = random.randint(8, 13)
 
 log = {
     'damage dealt': None,
@@ -22,7 +22,9 @@ def fight():
     dmg = random.randint(2, 5)
     enemy_hp -= random.randint(2, 5)
     log['damage dealt'] = dmg
-    if enemy_hp < 0:
+    print('Damage dealt:',dmg)
+    if enemy_hp <= 0:
+        print('Enemy died')
         create_new_enemy()
 
 
@@ -31,14 +33,25 @@ def heal():
     healing = random.randint(20, 30)
     hp += healing
     log['health healed'] = healing
-
+    if hp > 100:
+        hp = 100
 
 def enemy_attack():
     global hp
     dmg = random.randint(15, 35)
     hp -= dmg
     log['damage taken'] = dmg
+    print('Damage taken:',dmg)
 
+while hp > 0:
+    player_option = input('Whatchu gon do?')
+    if player_option == '1':
+        fight()
+    if player_option == '2':
+        heal()
+    enemy_attack()
+    def turn_end():
+        print('Health:',hp)
+        print('Enemy health:',enemy_hp)
 
-create_new_enemy()
-print(enemy_hp)
+print('Oh! You died lol. But you killed:',kill_count,'peeple tho')
